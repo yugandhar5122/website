13ba2fe93a928882d984d6c6f334633c3fc35094
yugandhar5122 <yugandharreddymanukonda1@gmail.com>
2024-09-01 12:12:21 +0530

    Create knowledgebase.js

---
+++ knowledgebase.js
@@ -0,0 +1,11 @@
+// knowledgeBase.js
+export const knowledgeBase = {
+  "hey hi hello": "hello I was the executive assistant of the school let me know your querys",
+  " name": "XYZ School",
+  "time, school, start": "8:30am to 4:00pm",
+  "located, address, adress": "The school is located at ABS near DEF",
+  "library": "Yes, the school provides a library with a wide range of books",
+  "bus": "We provide bus service for long-distance students",
+  "contact, phone mobile, contact info, phone number talk directly ": "You can contact us through mobilenumber xxxxxx54 email id xxxxxxxxx@gmail.com",
+  "principal": "My name. Contact xxxxxxx43"
+};
